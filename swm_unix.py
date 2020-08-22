726e309861ffafdd2bc68b3103560b5265071064
anthony-chang <anthony20093@gmail.com>
2020-08-22 13:02:11 -0400

    use abspath for id key

--- swm_unix.py
+++ swm_unix.py
@@ -61,38 +61,31 @@ class Handler(FileSystemEventHandler):
         if event.event_type == 'created':
             # Take any action here when a file is first created.
             print("Received created event - %s." % event.src_path)
-            file = os.path.basename(event.src_path)
+            file = os.path.abspath(event.src_path)
             fileIds[file] = creation_date(filePath)
             fId = fileIds[file]
-            # TODO: send event to create
 
         elif event.event_type == 'deleted':
             # Taken any action here when a file is modified.
             print("Received deleted event - %s." % event.src_path)
-            file = os.path.basename(event.src_path)
+            file = os.path.abspath(event.src_path)
             fileIds.pop(file)
-
-            # TODO: send event to delete
 
         elif event.event_type == 'modified':
             # Taken any action here when a file is modified.
             print("Received modified event - %s." % event.src_path)
-            file = os.path.basename(event.src_path)
+            file = os.path.abspath(event.src_path)
             fId = fileIds[file]
-
-            # TODO: send event to update
 
         elif event.event_type == 'moved':
             # Taken any action here when a file is moved.
             print("Received moved event - %s." % event.src_path)
-            file = os.path.basename(event.dest_path)
+            file = os.path.abspath(event.dest_path)
             fId = fileIds[file]
             fileIds.pop(file)
 
-            # TODO: send event to remove
-            new_file = os.path.basename(event.dest_path)
+            new_file = os.path.abspath(event.dest_path)
             fId = fileIds[file]
-            # TODO: send event to add
 
         if fId and os.path.isfile(filePath):
             updateFile(event.event_type, fId, filePath)
