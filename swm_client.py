5a1c01335086ae2b092e8a6fca77ed057702a3aa
anthony-chang <anthony20093@gmail.com>
2020-08-21 22:40:20 -0400

    detect when changes are made to a selected dir

--- swm_client.py
+++ swm_client.py
@@ -1,18 +1,59 @@
+
 import os
-import subprocess
-FILEBROWSER_PATH = os.path.join(os.getenv('WINDIR'), 'explorer.exe')
+import win32file
+import win32con
+from tkinter import filedialog
+from tkinter import *
 
-def open_explorer(path):
-    # explorer would choke on forward slashes
-    path = os.path.normpath(path)
+ACTIONS = {
+    1 : "Created",
+    2 : "Deleted",
+    3 : "Modified",
+    4 : "Renamed from",
+    5 : "to"
+}
+FILE_LIST_DIRECTORY = 0x0001
 
-    if os.path.isdir(path):
-        subprocess.run([FILEBROWSER_PATH, path])
-    elif os.path.isfile(path):
-        subprocess.run([FILEBROWSER_PATH, '/select,', os.path.normpath(path)])
+def getDir():
+    root = Tk()
+    root.withdraw()
+    selectedDir = filedialog.askdirectory(initialdir=os.getcwd())
+    return selectedDir
+
+def workloop(dir):
+    print("Running SWM client in " + dir)
+    dirHandle = win32file.CreateFile(
+                dir, 
+                FILE_LIST_DIRECTORY,
+                win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
+                None, 
+                win32con.OPEN_EXISTING,
+                win32con.FILE_FLAG_BACKUP_SEMANTICS,
+                None)
+
+    while 1:
+        results = win32file.ReadDirectoryChangesW(
+            dirHandle,
+            4096,
+            True,
+            win32con.FILE_NOTIFY_CHANGE_FILE_NAME  |
+            win32con.FILE_NOTIFY_CHANGE_DIR_NAME   |
+            win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+            win32con.FILE_NOTIFY_CHANGE_SIZE       |
+            win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+            win32con.FILE_NOTIFY_CHANGE_SECURITY,
+            None,
+            None)
+        for action, file in results:
+            fullPath = os.path.join(dir, file)
+            print(ACTIONS.get(action, "Unknown"), fullPath)
+
 
 def main():
-    open_explorer(os.getcwd())
+    dir = getDir()
+    workloop(dir)
+
+
 
 if __name__ == "__main__":
     main()
